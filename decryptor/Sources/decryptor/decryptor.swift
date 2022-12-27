adcdccf63625371912632ecc03c4149488f3d6d5
Matt Lilley <matt.lilley@securitease.com>
2022-12-27 07:43:14 +0000

    Refactor to use functions and decode the name of the device if it has been seen

--- decryptor/Sources/decryptor/decryptor.swift
+++ decryptor/Sources/decryptor/decryptor.swift
@@ -28,33 +28,56 @@ public struct decryptor {
             let filesURL = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("Library/com.apple.icloud.searchpartyd/OwnedBeacons")
             let files = try FileManager.default.contentsOfDirectory(at: filesURL, includingPropertiesForKeys: nil)
             for url in files {
-                let isAccessing = url.startAccessingSecurityScopedResource()
-                let recordData = try! Data(contentsOf: url)
-                if (isAccessing) {
-                    url.stopAccessingSecurityScopedResource()
-                }
-                if let arr = try! PropertyListSerialization.propertyList(from: recordData, options: [], format: nil) as? [Any] {
-                    let nonce = arr[0] as! NSData
-                    let tag = arr[1] as! NSData
-                    let ciphertext = arr[2] as! NSData
-                    let sealedBox = try! AES.GCM.SealedBox(nonce: AES.GCM.Nonce(data: nonce),
-                                                        ciphertext: ciphertext,
-                                                        tag: tag)
-                    let key = SymmetricKey(data: passwordData)
-                    let decryptedData = try! AES.GCM.open(sealedBox, using: key)
-                    if let decrypted = try! PropertyListSerialization.propertyList(from: decryptedData, options: [], format: nil) as? [String:Any] {
-                        let pairingDate = decrypted["pairingDate"] as! Date
-                        let id = decrypted["identifier"] as! String
-                        let sharedSecret = ((((decrypted["sharedSecret"] as! [String:Any]) ["key"]) as! [String:Any]) ["data"]! as! Data).hexEncodedString()
-                        let privateKey = ((((decrypted["privateKey"] as! [String:Any]) ["key"]) as! [String:Any]) ["data"]! as! Data).hexEncodedString()
-                        print("\(formatter.string(from:pairingDate)) \(sharedSecret) \(privateKey) Device \(id)")
-                    }
-                }
-
+                let decrypted = decrypt(file: url, usingPassword:passwordData) as! [String:Any]
+                let pairingDate = decrypted["pairingDate"] as! Date
+                let id = decrypted["identifier"] as! String
+                let name = findName(from: id, usingPassword: passwordData)
+                let sharedSecret = ((((decrypted["sharedSecret"] as! [String:Any]) ["key"]) as! [String:Any]) ["data"]! as! Data).hexEncodedString()
+                let privateKey = ((((decrypted["privateKey"] as! [String:Any]) ["key"]) as! [String:Any]) ["data"]! as! Data).hexEncodedString()
+                print("\(formatter.string(from:pairingDate)) \(sharedSecret) \(privateKey) \(name)")
             }
         } catch {
             print(error)
         }
+    }
+
+    static func findName(from id:String, usingPassword passwordData:Data) -> String {
+        let filesURL = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("Library/com.apple.icloud.searchpartyd/BeaconNamingRecord/\(id)")
+        do {
+            let files = try FileManager.default.contentsOfDirectory(at: filesURL, includingPropertiesForKeys: nil)
+            for url in files {
+                let decrypted = decrypt(file: url, usingPassword:passwordData) as! [String:Any]
+                if let emoji = decrypted["emoji"] {
+                    return "\(decrypted["name"]!) (\(emoji))"
+                }
+                return decrypted["name"] as! String
+            }
+        } catch {
+            // File doesnt exist
+            return "Unknown device \(id)"
+        }
+        return "Unknown device \(id)"
+    }
+
+    static func decrypt(file url:URL, usingPassword passwordData:Data) -> Any {
+        let isAccessing = url.startAccessingSecurityScopedResource()
+        let recordData = try! Data(contentsOf: url)
+        if (isAccessing) {
+            url.stopAccessingSecurityScopedResource()
+        }
+        if let arr = try! PropertyListSerialization.propertyList(from: recordData, options: [], format: nil) as? [Any] {
+            let nonce = arr[0] as! NSData
+            let tag = arr[1] as! NSData
+            let ciphertext = arr[2] as! NSData
+            let sealedBox = try! AES.GCM.SealedBox(nonce: AES.GCM.Nonce(data: nonce),
+                                                ciphertext: ciphertext,
+                                                tag: tag)
+            let key = SymmetricKey(data: passwordData)
+            let decryptedData = try! AES.GCM.open(sealedBox, using: key)
+            let plist = try! PropertyListSerialization.propertyList(from: decryptedData, options: [], format: nil)
+            return plist
+        }
+        return []
     }
 }
 
